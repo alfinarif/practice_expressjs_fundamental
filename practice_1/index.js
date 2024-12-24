70b5b2b09385ad1bb09aeca300321edfe4580d2a
alfinarif <cleavercode@gmail.com>
2024-12-25 02:44:13 +0300

    expressJs routing

--- practice_1/index.js
+++ practice_1/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 
-app = express();
+const app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello ExpressJs");
+// GET
+app.get('/', (req, res)=>{
+    res.send("Home page route")
+});
+
+// POST
+app.post('/about', (req, res)=>{
+    res.send("About page route")
 })
 
+//PUT
+app.put('/contact', (req, res)=>{
+    res.send("Contact page route")
+})
 
-app.listen(8000, function (){
-    console.log('Server Run Success');
+//DELETE
+app.delete('/terms', (req, res)=>{
+    res.send("Terms page route")
+})
+
+app.listen(8000, ()=>{
+    console.log('Server is run on port localhost:80000');
 })
